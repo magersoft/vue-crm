e7c276a8601bc5f1519cb5a3f669cef85d45e2be
magersoft <magervlad@yandex.ru>
2019-06-11 19:34:09 +0300

    install firebase

--- src/main.js
+++ src/main.js
@@ -1,13 +1,41 @@
 import Vue from 'vue';
+import Vuelidate from 'vuelidate';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import dateFilter from '@/filters/date.filter';
+import messagePlugin from '@/utils/message.plugin';
 import './registerServiceWorker';
+import 'materialize-css/dist/js/materialize.min';
+
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
 
 Vue.config.productionTip = false;
 
-new Vue({
-  router,
-  store,
-  render: h => h(App),
-}).$mount('#app');
+Vue.use(Vuelidate);
+Vue.use(messagePlugin);
+Vue.filter('date', dateFilter);
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyDUvcpLRkqAf8rml-jZkwS9zJEKqs7_wmw',
+  authDomain: 'vue-crm-289d4.firebaseapp.com',
+  databaseURL: 'https://vue-crm-289d4.firebaseio.com',
+  projectId: 'vue-crm-289d4',
+  storageBucket: 'vue-crm-289d4.appspot.com',
+  messagingSenderId: '381558199116',
+  appId: '1:381558199116:web:b8eb30a142058170',
+});
+
+let app;
+
+firebase.auth().onAuthStateChanged(() => {
+  if (!app) {
+    app = new Vue({
+      router,
+      store,
+      render: h => h(App),
+    }).$mount('#app');
+  }
+});
---
+++ src/utils/message.plugin.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+export default {
+  install(Vue, options) {
+    Vue.prototype.$message = function (text) {
+      M.toast({ html: text });
+    };
+
+    Vue.prototype.$error = function (html) {
+      M.toast({ html: `[Ошибка]: ${html}` });
+    };
+  },
+};
