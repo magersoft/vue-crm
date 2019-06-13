df2e033e51c7e4aad9637c67fd15cb83a57d5555
magersoft <magervlad@yandex.ru>
2019-06-13 17:33:04 +0300

    create/update categories

--- src/main.js
+++ src/main.js
@@ -4,7 +4,9 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import dateFilter from '@/filters/date.filter';
+import currencyFilter from '@/filters/currency.filter';
 import messagePlugin from '@/utils/message.plugin';
+import Loader from '@/components/app/Loader';
 import './registerServiceWorker';
 import 'materialize-css/dist/js/materialize.min';
 
@@ -17,6 +19,8 @@ Vue.config.productionTip = false;
 Vue.use(Vuelidate);
 Vue.use(messagePlugin);
 Vue.filter('date', dateFilter);
+Vue.filter('currency', currencyFilter);
+Vue.component('Loader', Loader);
 
 firebase.initializeApp({
   apiKey: 'AIzaSyDUvcpLRkqAf8rml-jZkwS9zJEKqs7_wmw',
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import auth from './auth';
+import info from './info';
+import category from './category';
 
 Vue.use(Vuex);
 
@@ -20,9 +22,15 @@ export default new Vuex.Store({
     error: s => s.error,
   },
   actions: {
-
+    async fetchCurrency() {
+      const key = process.env.VUE_APP_FIXER;
+      const response = await fetch(`http://data.fixer.io/api/latest?access_key=${key}&symbols=USD,EUR,RUB`);
+      return response.json();
+    }
   },
   modules: {
     auth,
+    info,
+    category,
   },
 });
